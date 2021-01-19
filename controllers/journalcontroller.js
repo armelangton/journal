08fb4ae7064b4a87eb11784285ae08c8c20adece
ARMELANGTON <you@example.com>
2021-01-19 15:04:37 -0500

    creating journal post

--- controllers/journalcontroller.js
+++ controllers/journalcontroller.js
@@ -1,8 +1,26 @@
-let express = require('express');
-let router = express.Router();
-router.get('/practice', validateSession, function(req, res) {
+const express = require ('express');
+const router = express.Router();
+const valaidateSession = require('../middleware/validate-session');
+const Journal =require('../db').import('../models/journal');
 
-    res.send("Hey!!! This is a practice route!");
-});
+router.get('/practice', validateSession, function (req,res)
 
-module.exports = router;
+res.send('Hey!! This is a practice route!') });
+
+
+
+/************
+ * JOURNAL CREATE
+ *//**************/
+ router.post('create', validateSession, (req, res) => {
+     const journalEntry ={
+        title: req.body.journal.title,
+        date: req.body.journal.date,
+        entry: req.body.journal.etry,
+        owner: req.user.id 
+        }
+        Journal.create(journalEntry)
+        .then(journal => res.status(200).json(journal))
+    .catch(err=> res.status(500).json ({error: err} ))
+})
+module.exports =router;
